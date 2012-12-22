fd64eaeafbc8cc3decab64728616c768c9c605e6
Fuji, Goro <g.psy.va@gmail.com>
2012-12-22 23:14:07 +0900

    fix a compiler crash
    the return type of FunctionType may not be determined if
    the function type is FunctionChoiseType.

--- src/expression.jsx
+++ src/expression.jsx
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2012 DeNA Co., Ltd.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -47,7 +47,7 @@ abstract class Expression implements Stashable {
 
 	abstract function clone () : Expression;
 	abstract function serialize () : variant;
-	
+
 	function instantiate (instantiationContext : InstantiationContext) : boolean {
 		function onExpr(expr : Expression) : boolean {
 			if (expr instanceof NewExpression) {
@@ -1976,7 +1976,13 @@ class CallExpression extends OperatorExpression {
 		var type = this._expr.getType();
 		if (type == null)
 			return null;
-		return (type.resolveIfNullable() as ResolvedFunctionType).getReturnType();
+		var functionType = type.resolveIfNullable() as FunctionType;
+		if (functionType instanceof ResolvedFunctionType) {
+			return (functionType as ResolvedFunctionType).getReturnType();
+		}
+		else {
+			return null;
+		}
 	}
 
 	override function forEachExpression (cb : function(:Expression,:function(:Expression):void):boolean) : boolean {
@@ -2025,7 +2031,7 @@ class SuperExpression extends OperatorExpression {
 	function getFunctionType () : FunctionType {
 		return this._funcType;
 	}
-		
+
 	override function serialize () : variant {
 		return [
 			"SuperExpression",
